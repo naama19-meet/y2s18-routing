5cc7335a3bf4792aff71386f1d98aeb41eb941b4
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 14:13:24 +0300

    wab divalomnet

--- exercises/app.py
+++ exercises/app.py
@@ -4,6 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+    return render_template("home.html")
+
+@app.route('/student_id/<int:student_id>')
+def display_student(student_id):
+    find = query_by_id(student_id)
+
+    return render_template('student.html', student_id=student_id, student=find)
+
 
 app.run(debug=True)
